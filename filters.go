e98ac9c8b8c0921a2f109656794a76b3e2c098d9
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-17 23:00:00 +0300

    Add the htmlselector.OptimizeFilters() function

--- filters.go
+++ filters.go
@@ -6,8 +6,26 @@ type TagName string
 // AttributeName ...
 type AttributeName string
 
+// FilterGroup ...
+type FilterGroup map[TagName][]AttributeName
+
 // OptimizedFilterGroup ...
 type OptimizedFilterGroup map[TagName]OptimizedAttributeFilterGroup
 
 // OptimizedAttributeFilterGroup ...
 type OptimizedAttributeFilterGroup map[AttributeName]struct{}
+
+// OptimizeFilters ...
+func OptimizeFilters(filters FilterGroup) OptimizedFilterGroup {
+	optimizedFilters := make(OptimizedFilterGroup)
+	for tag, attributes := range filters {
+		optimizedAttributeFilters := make(OptimizedAttributeFilterGroup)
+		for _, attribute := range attributes {
+			optimizedAttributeFilters[attribute] = struct{}{}
+		}
+
+		optimizedFilters[tag] = optimizedAttributeFilters
+	}
+
+	return optimizedFilters
+}
