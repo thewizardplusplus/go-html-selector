eea24914d0842e39097d85ab4c3afea5cc78f7b1
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-18 23:00:00 +0300

    Test unmarshalling from JSON for the htmlselector.FilterGroup type

--- filters_test.go
+++ filters_test.go
@@ -1,10 +1,60 @@
 package htmlselector
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestFilterGroup_Unmarshal(test *testing.T) {
+	type args struct {
+		data []byte
+	}
+
+	for _, data := range []struct {
+		name string
+		args args
+		want FilterGroup
+	}{
+		{
+			name: "without filters",
+			args: args{
+				data: []byte("null"),
+			},
+			want: nil,
+		},
+		{
+			name: "with an empty filter",
+			args: args{
+				data: []byte(`{"a": null}`),
+			},
+			want: FilterGroup{"a": nil},
+		},
+		{
+			name: "with a nonempty filter",
+			args: args{
+				data: []byte(`{"a": ["href", "title"]}`),
+			},
+			want: FilterGroup{"a": {"href", "title"}},
+		},
+		{
+			name: "with few filters",
+			args: args{
+				data: []byte(`{"a": ["href", "title"], "img": ["src", "alt"]}`),
+			},
+			want: FilterGroup{"a": {"href", "title"}, "img": {"src", "alt"}},
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			var filters FilterGroup
+			err := json.Unmarshal(data.args.data, &filters)
+
+			assert.Equal(test, data.want, filters)
+			assert.NoError(test, err)
+		})
+	}
+}
 
 func TestOptimizeFilters(test *testing.T) {
 	type args struct {
