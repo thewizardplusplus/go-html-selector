d8b2c1deeb11c64fcf1215355c67cef92a797568
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-28 23:00:00 +0300

    Add tests for the byteutils.Copy() function

---
+++ byte-utils/byte_utils_test.go
@@ -0,0 +1,66 @@
+package byteutils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCopy(test *testing.T) {
+	type args struct {
+		bytes []byte
+	}
+
+	for _, data := range []struct {
+		name         string
+		args         args
+		action       func(bytes []byte)
+		wantOriginal []byte
+		wantCopy     []byte
+	}{
+		{
+			name: "nil",
+			args: args{
+				bytes: nil,
+			},
+			action:       func(bytes []byte) {},
+			wantOriginal: nil,
+			wantCopy:     []byte{},
+		},
+		{
+			name: "empty",
+			args: args{
+				bytes: []byte{},
+			},
+			action:       func(bytes []byte) {},
+			wantOriginal: []byte{},
+			wantCopy:     []byte{},
+		},
+		{
+			name: "nonempty/without changes",
+			args: args{
+				bytes: []byte("test"),
+			},
+			action:       func(bytes []byte) {},
+			wantOriginal: []byte("test"),
+			wantCopy:     []byte("test"),
+		},
+		{
+			name: "nonempty/with changes",
+			args: args{
+				bytes: []byte("test"),
+			},
+			action:       func(bytes []byte) { bytes[2] = 'x' },
+			wantOriginal: []byte("text"),
+			wantCopy:     []byte("test"),
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			got := Copy(data.args.bytes)
+			data.action(data.args.bytes)
+
+			assert.Equal(test, data.wantOriginal, data.args.bytes)
+			assert.Equal(test, data.wantCopy, got)
+		})
+	}
+}
