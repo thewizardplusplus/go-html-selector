61e21317f95de51807f588609280d0410e6e9ff7
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-30 23:00:00 +0300

    Add the htmlselector.SkipEmptyAttributes() option

--- options.go
+++ options.go
@@ -2,7 +2,8 @@ package htmlselector
 
 // OptionConfig ...
 type OptionConfig struct {
-	skipEmptyTags bool
+	skipEmptyTags       bool
+	skipEmptyAttributes bool
 }
 
 // Option ...
@@ -12,6 +13,13 @@ type Option func(config *OptionConfig)
 func SkipEmptyTags() Option {
 	return func(config *OptionConfig) {
 		config.skipEmptyTags = true
+	}
+}
+
+// SkipEmptyAttributes ...
+func SkipEmptyAttributes() Option {
+	return func(config *OptionConfig) {
+		config.skipEmptyAttributes = true
 	}
 }
 
