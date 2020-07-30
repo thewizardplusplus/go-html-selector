7c2540da60d2d8b430ba3c50916ca9460449049b
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-30 23:00:00 +0300

    Add tests for the byteutils.String() function

--- byte-utils/byte_utils_test.go
+++ byte-utils/byte_utils_test.go
@@ -64,3 +64,62 @@ func TestCopy(test *testing.T) {
 		})
 	}
 }
+
+func TestString(test *testing.T) {
+	type args struct {
+		bytes []byte
+	}
+
+	for _, data := range []struct {
+		name         string
+		args         args
+		action       func(bytes []byte)
+		wantOriginal []byte
+		wantString   string
+	}{
+		{
+			name: "nil",
+			args: args{
+				bytes: nil,
+			},
+			action:       func(bytes []byte) {},
+			wantOriginal: nil,
+			wantString:   "",
+		},
+		{
+			name: "empty",
+			args: args{
+				bytes: []byte{},
+			},
+			action:       func(bytes []byte) {},
+			wantOriginal: []byte{},
+			wantString:   "",
+		},
+		{
+			name: "nonempty/without changes",
+			args: args{
+				bytes: []byte("test"),
+			},
+			action:       func(bytes []byte) {},
+			wantOriginal: []byte("test"),
+			wantString:   "test",
+		},
+		{
+			name: "nonempty/with changes",
+			args: args{
+				bytes: []byte("test"),
+			},
+			action:       func(bytes []byte) { bytes[2] = 'x' },
+			wantOriginal: []byte("text"),
+			wantString:   "text",
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			got := String(data.args.bytes)
+			data.action(data.args.bytes)
+
+			assert.Equal(test, data.wantOriginal, data.args.bytes)
+			assert.Equal(test, data.wantString, got)
+		})
+	}
+}
