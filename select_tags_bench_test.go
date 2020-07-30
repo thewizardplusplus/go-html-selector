bae4a0355753377a9f5803b01429a245715fa47c
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-07-30 23:00:00 +0300

    Add benchmarks with the builders.FlattenBuilder structure

--- select_tags_bench_test.go
+++ select_tags_bench_test.go
@@ -15,61 +15,84 @@ func BenchmarkSelectTags(benchmark *testing.B) {
 		filters    OptimizedFilterGroup
 	}
 
-	for _, data := range []struct {
-		name string
-		args args
+	for _, builder := range []struct {
+		name           string
+		implementation Builder
 	}{
 		{
-			name: "simple markup",
-			args: args{
-				makeMarkup: func(tagCount int) string {
-					var markupParts []string
-					for tagIndex := 0; tagIndex < tagCount; tagIndex++ {
-						markupParts = append(markupParts, fmt.Sprintf(
-							`<p><a href="http://example.com/%[1]d">%[1]d</a></p>`,
-							tagIndex,
-						))
-					}
-
-					return strings.Join(markupParts, "")
-				},
-				filters: OptimizedFilterGroup{"a": {"href": {}}},
-			},
+			name:           "structural builder",
+			implementation: new(builders.StructuralBuilder),
 		},
 		{
-			name: "complex markup",
-			args: args{
-				makeMarkup: func(tagCount int) string {
-					var markupParts []string
-					for tagIndex := 0; tagIndex < tagCount; tagIndex++ {
-						markupParts = append(markupParts, fmt.Sprintf(
-							`<p><a href="http://example.com/%[1]d" title="%[1]d">%[1]d</a></p>`+
-								`<p><img src="http://example.com/%[1]d" alt="%[1]d" /></p>`,
-							tagIndex,
-						))
-					}
-
-					return strings.Join(markupParts, "")
-				},
-				filters: OptimizedFilterGroup{
-					"a":   {"href": {}, "title": {}},
-					"img": {"src": {}, "alt": {}},
-				},
-			},
+			name:           "flatten builder",
+			implementation: new(builders.FlattenBuilder),
 		},
 	} {
-		for tagCount := 10; tagCount <= 1e6; tagCount *= 10 {
-			markup := data.args.makeMarkup(tagCount)
-			markupSize := bytefmt.ByteSize(uint64(len(markup)))
+		for _, data := range []struct {
+			name string
+			args args
+		}{
+			{
+				name: "simple markup",
+				args: args{
+					makeMarkup: func(tagCount int) string {
+						var markupParts []string
+						for tagIndex := 0; tagIndex < tagCount; tagIndex++ {
+							markupParts = append(markupParts, fmt.Sprintf(
+								`<p><a href="http://example.com/%[1]d">%[1]d</a></p>`,
+								tagIndex,
+							))
+						}
 
-			name := fmt.Sprintf("%s/%d tags/%s", data.name, tagCount, markupSize)
-			benchmark.Run(name, func(benchmark *testing.B) {
-				for i := 0; i < benchmark.N; i++ {
-					var builder builders.StructuralBuilder
-					reader := strings.NewReader(markup)
-					SelectTags(reader, data.args.filters, &builder) // nolint: errcheck
-				}
-			})
+						return strings.Join(markupParts, "")
+					},
+					filters: OptimizedFilterGroup{"a": {"href": {}}},
+				},
+			},
+			{
+				name: "complex markup",
+				args: args{
+					makeMarkup: func(tagCount int) string {
+						var markupParts []string
+						for tagIndex := 0; tagIndex < tagCount; tagIndex++ {
+							markupParts = append(markupParts, fmt.Sprintf(
+								`<p><a href="http://example.com/%[1]d" title="%[1]d">%[1]d</a></p>`+
+									`<p><img src="http://example.com/%[1]d" alt="%[1]d" /></p>`,
+								tagIndex,
+							))
+						}
+
+						return strings.Join(markupParts, "")
+					},
+					filters: OptimizedFilterGroup{
+						"a":   {"href": {}, "title": {}},
+						"img": {"src": {}, "alt": {}},
+					},
+				},
+			},
+		} {
+			for tagCount := 10; tagCount <= 1e6; tagCount *= 10 {
+				markup := data.args.makeMarkup(tagCount)
+				markupSize := bytefmt.ByteSize(uint64(len(markup)))
+
+				name := fmt.Sprintf(
+					"%s/%s/%d tags/%s",
+					builder.name,
+					data.name,
+					tagCount,
+					markupSize,
+				)
+				benchmark.Run(name, func(benchmark *testing.B) {
+					for i := 0; i < benchmark.N; i++ {
+						reader := strings.NewReader(markup)
+						SelectTags( // nolint: errcheck
+							reader,
+							data.args.filters,
+							builder.implementation,
+						)
+					}
+				})
+			}
 		}
 	}
 }
