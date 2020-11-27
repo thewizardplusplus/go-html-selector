8faeaa1960aac596a3ecf1726cb4c336880b637a
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-11-27 23:00:00 +0300

    Add the example with the universal tag

--- examples_test.go
+++ examples_test.go
@@ -131,3 +131,67 @@ func ExampleSelectTags_withFlattenBuilder() {
 	// "http://example.com/2.1"
 	// "http://example.com/2.2"
 }
+
+func ExampleSelectTags_withUniversalTag() {
+	reader := strings.NewReader(`
+		<ul>
+			<li>
+				<a href="http://example.com/1" title="link #1">1</a>
+				<video
+					src="http://example.com/1.1"
+					poster="http://example.com/1.2"
+					title="video #1">
+				</video>
+			</li>
+			<li>
+				<a href="http://example.com/2" title="link #2">2</a>
+				<video
+					src="http://example.com/2.1"
+					poster="http://example.com/2.2"
+					title="video #2">
+				</video>
+			</li>
+		</ul>
+	`)
+
+	filters := htmlselector.OptimizeFilters(htmlselector.FilterGroup{
+		htmlselector.UniversalTag: {"title"},
+		"a":                       {"href"},
+		"video":                   {"src", "poster"},
+	})
+
+	var builder builders.StructuralBuilder
+	err := htmlselector.SelectTags(
+		reader,
+		filters,
+		&builder,
+		htmlselector.SkipEmptyTags(),
+		htmlselector.SkipEmptyAttributes(),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, tag := range builder.Tags() {
+		fmt.Printf("<%s>:\n", tag.Name)
+		for _, attribute := range tag.Attributes {
+			fmt.Printf("  %s=%q\n", attribute.Name, attribute.Value)
+		}
+	}
+
+	// Output:
+	// <a>:
+	//   href="http://example.com/1"
+	//   title="link #1"
+	// <video>:
+	//   src="http://example.com/1.1"
+	//   poster="http://example.com/1.2"
+	//   title="video #1"
+	// <a>:
+	//   href="http://example.com/2"
+	//   title="link #2"
+	// <video>:
+	//   src="http://example.com/2.1"
+	//   poster="http://example.com/2.2"
+	//   title="video #2"
+}
