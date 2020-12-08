edfd1ab766b7c5e5facf111f972d73ae2e8ca731
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-08 23:00:00 +0300

    Add the builders.TextBuilder.AddText() method

--- builders/text_builder.go
+++ builders/text_builder.go
@@ -1,5 +1,9 @@
 package builders
 
+import (
+	byteutils "github.com/thewizardplusplus/go-html-selector/byte-utils"
+)
+
 // TextBuilder ...
 type TextBuilder struct {
 	textParts [][]byte
@@ -8,4 +12,10 @@ type TextBuilder struct {
 // TextParts ...
 func (builder TextBuilder) TextParts() [][]byte {
 	return builder.textParts
+}
+
+// AddText ...
+func (builder *TextBuilder) AddText(text []byte) {
+	textCopy := byteutils.Copy(text)
+	builder.textParts = append(builder.textParts, textCopy)
 }
