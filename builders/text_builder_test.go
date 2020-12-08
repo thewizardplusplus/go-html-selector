5a9fde543aa4aa4598c6324ad4a27bb24cafe4e1
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-08 23:00:00 +0300

    Add tests for the builders.TextBuilder.AddText() method

--- builders/text_builder_test.go
+++ builders/text_builder_test.go
@@ -17,3 +17,20 @@ func TestTextBuilder_TextParts(test *testing.T) {
 
 	assert.Equal(test, builder.textParts, gotTextParts)
 }
+
+func TestTextBuilder_AddText(test *testing.T) {
+	builder := TextBuilder{
+		textParts: [][]byte{
+			[]byte("text part #1"),
+			[]byte("text part #2"),
+		},
+	}
+	builder.AddText([]byte("text part #3"))
+
+	wantTextParts := [][]byte{
+		[]byte("text part #1"),
+		[]byte("text part #2"),
+		[]byte("text part #3"),
+	}
+	assert.Equal(test, wantTextParts, builder.textParts)
+}
