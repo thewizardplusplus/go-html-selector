d758884fd5dda43fbee0612e6e5cf0401152cc14
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-11 23:00:00 +0300

    Add the htmlselector.selector.selectTag() method

--- selector.go
+++ selector.go
@@ -3,6 +3,7 @@ package htmlselector
 import (
 	"io"
 
+	byteutils "github.com/thewizardplusplus/go-html-selector/byte-utils"
 	"golang.org/x/net/html"
 )
 
@@ -15,4 +16,31 @@ func newSelector(reader io.Reader) selector {
 	return selector{
 		tokenizer: tokenizer,
 	}
+}
+
+func (selector selector) selectTag(
+	filters OptimizedFilterGroup,
+	additionalAttributeFilters OptimizedAttributeFilterGroup,
+	builder Builder,
+	config OptionConfig,
+) {
+	name, hasAttributes := selector.tokenizer.TagName()
+	attributeFilters, ok := filters[TagName(byteutils.String(name))]
+	if !ok && len(additionalAttributeFilters) == 0 {
+		return
+	}
+
+	attributeCount := selectAttributes(
+		selector.tokenizer,
+		hasAttributes,
+		attributeFilters,
+		additionalAttributeFilters,
+		builder,
+		config,
+	)
+	if config.skipEmptyTags && attributeCount == 0 {
+		return
+	}
+
+	builder.AddTag(name)
 }
