81a8fa83b088ea04b588be4c7bcabfdc526b3b85
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-12-11 23:00:00 +0300

    Add the htmlselector.newSelector() function

--- selector.go
+++ selector.go
@@ -1,9 +1,18 @@
 package htmlselector
 
 import (
+	"io"
+
 	"golang.org/x/net/html"
 )
 
 type selector struct {
 	tokenizer *html.Tokenizer
+}
+
+func newSelector(reader io.Reader) selector {
+	tokenizer := html.NewTokenizer(reader)
+	return selector{
+		tokenizer: tokenizer,
+	}
 }
